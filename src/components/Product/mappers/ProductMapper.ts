05c101662101803cbe24bc1950f76acd5b0e9df2
Adrian Olszowski <aol@spyro-soft.com>
2021-01-24 20:22:16 +0100

    remove unneccessary code

--- src/components/Product/mappers/ProductMapper.ts
+++ src/components/Product/mappers/ProductMapper.ts
@@ -1,21 +1,4 @@
 const getProduct = async (product: any) => {
-  // const productData = product.data()
-  // const images: string[] = []
-  // let mainImage = ''
-  // // if (productData.image) {
-  // //   for (const image of productData.image) {
-  // //     const nextImage = await getImage(image)
-  // //     if (image === productData.mainImage) mainImage = nextImage
-  // //     images.push(nextImage)
-  // //   }
-  // }
-  // const mappedProduct: ProductInterface = {
-  //   ...productData,
-  //   id: product.id,
-  //   images,
-  //   image: mainImage
-  // }
-
   return product.img
 }
 
